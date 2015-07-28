70e3f86f519921011a1c3c7997812009d62ec4ca
Carl Smith <carl@afterburnerimages.com>
2015-07-28 13:20:38 -0700

    Removed totalQuantity and unitOfMeasure vars Added findGroceryListItemWithName function

--- SwiftRecipeParser/Model/GroceryListItem.swift
+++ SwiftRecipeParser/Model/GroceryListItem.swift
@@ -2,7 +2,7 @@
 //  GroceryListItem.swift
 //  SwiftRecipeParser
 //
-//  Created by CarlSmith on 6/3/15.
+//  Created by CarlSmith on 7/7/15.
 //  Copyright (c) 2015 CarlSmith. All rights reserved.
 //
 
@@ -12,10 +12,17 @@ import CoreData
 class GroceryListItem: NSManagedObject {
 
     @NSManaged var cost: NSNumber
-    @NSManaged var name: String
-    @NSManaged var totalQuantity: NSNumber
-    @NSManaged var unitOfMeasure: String
     @NSManaged var isBought: NSNumber
+    @NSManaged var name: String
     @NSManaged var inGroceryList: GroceryList
 
+    class func findGroceryListItemWithName(name: String) -> GroceryListItem? {
+        if let groceryListItems = DatabaseInterface().entitiesOfType("GroceryListItem", predicate: NSPredicate(format: "name MATCHES %@", name)) as? Array<GroceryListItem> {
+            if groceryListItems.count == 1 {
+                return groceryListItems.first
+            }
+        }
+        return nil
+    }
+    
 }
