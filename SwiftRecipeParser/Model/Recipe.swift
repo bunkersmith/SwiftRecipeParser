25c6b84a8bce8cd624193238587b153808d08463
Carl Smith <carl@afterburnerimages.com>
2015-07-28 13:22:50 -0700

    Added title var Adjusted recipeDescription function accordingly

--- SwiftRecipeParser/Model/Recipe.swift
+++ SwiftRecipeParser/Model/Recipe.swift
@@ -2,8 +2,8 @@
 //  Recipe.swift
 //  SwiftRecipeParser
 //
-//  Created by CarlSmith on 7/26/14.
-//  Copyright (c) 2014 CarlSmith. All rights reserved.
+//  Created by CarlSmith on 6/4/15.
+//  Copyright (c) 2015 CarlSmith. All rights reserved.
 //
 
 import Foundation
@@ -11,18 +11,17 @@ import CoreData
 
 class Recipe: NSManagedObject {
 
-    @NSManaged var indexCharacter: String
     @NSManaged var instructions: String
-    @NSManaged var name: String
     @NSManaged var notes: String
     @NSManaged var servings: NSNumber
     @NSManaged var containsIngredients: NSOrderedSet
-
+    @NSManaged var title: RecipeTitle
+    
     func recipeDescription() -> NSString {
-        var returnValue: String = "\name: \(name)"
+        var returnValue: String = "\name: \(title.name)"
         returnValue += "\ninstructions: \(instructions)"
         returnValue += "\nnotes: \(notes)"
-        returnValue += "\nindexCharacter: \(indexCharacter)"
+        returnValue += "\nindexCharacter: \(title.indexCharacter)"
         returnValue += "\nservings: \(servings.intValue)"
         returnValue += "\ningredient count: \(containsIngredients.count)"
         
@@ -41,5 +40,4 @@ class Recipe: NSManagedObject {
         
         self.didChangeValueForKey("containsIngredients")
     }
-    
 }
