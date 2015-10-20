b39c2f96f0f70ecb788bf9803cb4d22a085a21a7
Carl Smith <carl@afterburnerimages.com>
2015-10-20 11:24:56 -0700

    Added application.shouldSaveApplicationState and application.shouldRestoreApplicationState functions (returning false for now)

--- SwiftRecipeParser/AppDelegate.swift
+++ SwiftRecipeParser/AppDelegate.swift
@@ -41,6 +41,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
+    func application(application: UIApplication, shouldSaveApplicationState coder: NSCoder) -> Bool {
+        return false
+    }
+    
+    func application(application: UIApplication, shouldRestoreApplicationState coder: NSCoder) -> Bool {
+        return false
+    }
 }
 
