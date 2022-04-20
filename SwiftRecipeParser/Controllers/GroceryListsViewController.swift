8189208657af3396ca029e228109f0828ea8e89c
Carl Smith <carl@afterburnerimages.com>
2022-04-20 00:02:20 -0700

    Made Buy button on GroceryListDetailVC easier to press

--- SwiftRecipeParser/Controllers/GroceryListsViewController.swift
+++ SwiftRecipeParser/Controllers/GroceryListsViewController.swift
@@ -1,6 +1,6 @@
 //
 //  GroceryListsViewController.swift
-//  SwiftR                                                                                                                                 ecipeParser
+//  SwiftRecipeParser
 //
 //  Created by CarlSmith on 6/1/15.
 //  Copyright (c) 2015 CarlSmith. All rights reserved.
